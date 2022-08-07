5d685e1adb18a3b4510c7ecfe9795e03b44ae5da
Dinesh K <dineshk.login@gmail.com>
2022-08-07 15:49:50 +0530

    initial commit

---
+++ sites/all/modules/custom/numeric_expansion/custom.js
@@ -0,0 +1,37 @@
+
+
+function test(value){
+
+    //alert(value);
+    if(value == 1){
+        document.getElementById("1").innerHTML = "one";
+
+}else if(value == 2){
+        document.getElementById("2").innerHTML = "two";
+ 
+}
+else if(value == 3){
+        document.getElementById("3").innerHTML = "three";
+ 
+}else if(value == 4){
+        document.getElementById("4").innerHTML = "four";
+ 
+}else if(value == 5){
+        document.getElementById("5").innerHTML = "five";
+ 
+}else if(value == 6){
+        document.getElementById("6").innerHTML = "six";
+}else if(value == 7){
+        document.getElementById("7").innerHTML = "seven";
+ 
+}else if(value == 8){
+        document.getElementById("8").innerHTML = "eight";
+ 
+}else if(value == 9){
+        document.getElementById("9").innerHTML = "nine";
+ 
+}else if(value == 10){
+        document.getElementById("10").innerHTML = "ten";
+ 
+}
+}
